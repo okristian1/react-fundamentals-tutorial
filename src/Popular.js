c8306f9b92f22cfc3105cf7cd341ffc1f58099a4
locrin <okristian1@gmail.com>
2017-04-21 01:11:14 +0200

    following tutorial

--- src/Popular.js
+++ src/Popular.js
@@ -1,40 +1,54 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+function SelectLanguage (props) {
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return (
+    <ul className='languages'>
+      {languages.map(function (lang) {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}>
+              {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
 
 class Popular extends React.Component {
   constructor(props) {
-    super(props);
+    super();
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
     };
+
     this.updateLanguage = this.updateLanguage.bind(this);
   }
-
   updateLanguage(lang) {
     this.setState(function () {
-      return (
-        selectedLanguage: lang
-      )
+      return {
+        selectedLanguage: lang,
+      }
     });
   }
-
-    render() {
-      var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-      return (
-        <ul className = "languages">
-        <p>Selected Language: {this.state.selectedLanguage}</p>
-        {languages.map(function(lang) {
-          return (
-            <li onClick={this.updateLanguage.bind(null, lang)}
-            key = {lang}>
-            {lang}
-            </li>
-          )
-        }, this)}
-        </ul>
-      )
-    }
+  render() {
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage} />
+      </div>
+    )
   }
-
-
+}
 
 module.exports = Popular;
