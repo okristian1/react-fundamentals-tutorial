f8b30f0b5b28e5c34b01d21d250f9a7913001450
locrin <okristian1@gmail.com>
2017-04-27 14:11:33 +0200

    hosting stuff

--- server/index.js
+++ server/index.js
@@ -3,9 +3,8 @@
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
-
